1c8720dbae078a8561ec9d78f1f5d631e04028b6
marhany <maarhany@gmail.com>
2018-04-17 21:58:52 -0300

    Commit Marhany

--- frontend/src/app/principal/principal.component.spec.ts
+++ frontend/src/app/principal/principal.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PrincipalComponent } from './principal.component';
 
+
 describe('PrincipalComponent', () => {
   let component: PrincipalComponent;
   let fixture: ComponentFixture<PrincipalComponent>;
